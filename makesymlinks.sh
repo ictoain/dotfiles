113aa8b385d2df7851cc2c5d7523c184f9dc8c42
Konrad Barth <konrad.barth@gmail.com>
2014-06-04 18:42:50 +0200

    Changing makesymlinks.sh so it can be rerun without error messages

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -14,9 +14,11 @@ files="bashrc vimrc zshrc screenrc screen oh-my-zsh"
 ##########
 
 # create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
-echo "done"
+if [ ! -d ~/$olddir ];then
+  echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+  mkdir -p $olddir
+  echo "done"
+fi
 
 # change to the dotfiles directory
 echo -n "Changing to the $dir directory ..."
@@ -27,14 +29,16 @@ echo -n "Pulling dotfiles repo..."
 git pull
 echo "done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+# move any existing dotfiles in homedir to dotfiles_old directory, then create 
+#symlinks from the homedir to any files in the ~/dotfiles directory specified
+#in $files
 for file in $files; do
-  echo "Moving any existing dotfiles from ~ to $olddir"
-  if [ -f ~/.$file ];then
-      mv ~/.$file ~/dotfiles_old/
+  if [ -f ~/.$file -o -d ~/.$file ];then
+    echo "Moving existing dotfile $file from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
   fi
-  echo "Creating symlink to $file in home directory."
-  ln -s $dir/$file ~/.$file
 done
 
 install_zsh () {
